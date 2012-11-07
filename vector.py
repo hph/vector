adecf7f8537b61fb5012729afa73f2bbcf1727f3
Haukur Páll Hallvarðsson <haukur@hph.(none)>
2012-11-07 19:03:38 +0000

    update

--- vector.py
+++ vector.py
@@ -13,33 +13,25 @@ class Vector():
         return Vector(self.x + other.x, self.y + other.y)
 
     def __iadd__(self, other):
-        self.x += other.x
-        self.y += other.y
-        return self
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
         return Vector(self.x - other.x, self.y - other.y)
 
     def __isub__(self, other):
-        self.x -= other.x
-        self.y -= other.y
-        return self
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other):
         return Vector(self.x * other, self.y * other)
 
     def __imul__(self, other):
-        self.x *= other
-        self.y *= other
-        return self
+        return Vector(self.x * other, self.y * other)
 
     def __div__(self, other):
         return Vector(self.x / other, self.y / other)
 
     def __idiv__(self, other):
-        self.x /= other
-        self.y /= other
-        return self
+        return Vector(self.x / other, self.y / other)
 
     def __neg__(self):
         return Vector(-self.x, -self.y)
@@ -50,28 +42,20 @@ class Vector():
     def __repr__(self):
         return 'Vector(%s, %s)' % (self.x, self.y)
 
-    def magnitude(self):
-        return _math.sqrt(pow(self.x, 2) + pow(self.y, 2))
+    def cross(self, other):
+        return self.x * other.y - self.y * other.x
+
+    def distance(self, other):
+        return _math.sqrt(pow(self.x - other.x, 2) + pow(self.y - other.y, 2))
 
     def dot(self, other):
         return self.x * other.x + self.y * other.y
 
-    def cross(self, other):
-        return self.x * other.y - self.y * other.x
-
-    def normalize(self):
-        try:
-            self.x /= self.magnitude()
-            self.y /= self.magnitude()
-        except ZeroDivisionError:
-            self.x = 0
-            self.y = 0
+    def magnitude(self):
+        return _math.sqrt(pow(self.x, 2) + pow(self.y, 2))
 
     def normalized(self):
         try:
             return self / self.magnitude()
         except ZeroDivisionError:
             return self
-
-    def distance(self, other):
-        return _math.sqrt(pow(self.x - other.x, 2) + pow(self.y - other.y, 2))
